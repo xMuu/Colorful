c41261241fbf797ab4e5ed728f13dadae783864f
Volio <volio.liu@gmail.com>
2016-03-05 20:10:47 +0800

    footer

--- footer.php
+++ footer.php
@@ -9,16 +9,16 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 
 </section>
 
-<footer class="mdl-mini-footer mdl-shadow--4dp">
+<footer class="mdl-mini-footer">
     <div class="mdl-mini-footer__left-section">
         <ul class="mdl-mini-footer__link-list">
             <li>&copy; <?php echo date('Y'); ?> <a href="<?php $this->options->siteUrl(); ?>"><strong><?php $this->options->title(); ?></strong></a>.</li>
         </ul>
     </div>
-    <div class="mdl-mini-footer__right-section">
+    <div class="mdl-mini-footer__right-section  mdl-layout--large-screen-only">
         <ul class="mdl-mini-footer__link-list">
             <li>Powered By <a href="http://www.typecho.org" rel="nofollow"><strong>Typecho</strong></a></li>
-            <li> | </li>
+            <li>|</li>
             <li>Theme By <a href="https://niconiconi.org"><strong>Volio</strong></a></li>
         </ul>
     </div>
